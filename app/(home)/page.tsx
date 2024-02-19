77f51f730d54b433eb932497e2b1d702a96113ae
natsume <natsumeneko831@outlook.com>
2024-02-19 12:59:35 +0800

    initialized

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my custom blog system!</h1>
+      <p>
+        This is a custom blog system built with Next.js and MDX. It's a work in
+        progress, but I hope you enjoy it!
+      </p>
+    </div>
+  );
+}
